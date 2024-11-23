3125a23545dd7252c8c02827aa3174eca38e7aa1
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-11-23 15:41:33 +0400

    test

--- apiConfig.ts
+++ apiConfig.ts
@@ -4,7 +4,7 @@ const apiURL = "https://studenv-2mo8x.ondigitalocean.app/";
 export const apiConfig = {
   baseUrl: `${apiURL}`,
   imagePath: `https://cdn.studenv.com/`,
-  mainUrl: "https://studenv.com",
+  mainUrl: "https://studenv.online",
 };
 
 export const api = axios.create({
