c9dff641e5db7b219aca10182801a6dd40eebde5
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-11-23 17:35:09 +0400

    test

--- apiConfig.ts
+++ apiConfig.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiURL = "https://studenv-2mo8x.ondigitalocean.app/";
+const apiURL = "https://studenv.online/api/";
 
 export const apiConfig = {
   baseUrl: `${apiURL}`,
